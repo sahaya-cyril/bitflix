33e17c34cf124bfa0974fc1fddf74bc752aec9d0
Cyril <sahayacyril2015@gmail.com>
2022-02-28 20:46:39 +0530

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Header from "./components/Header/Header";
+
 function App() {
   return (
     <div className="App">
-      <h1>HELLO!!</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import './Header.css';
+
+const Header = () => {
+    return <span className="header">🎥 BITFLIX 🎬</span>
+}
+
+export default Header;
